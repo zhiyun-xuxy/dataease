287e8f65a1f6d4118dacf9731ab5399c34c9b464
zhiyun-9527 <xuxy@cloud-cloud.com>
2024-05-24 14:21:14 +0800

    feat: DataEase 用户订阅管理员首页大屏内图表到自己的首页大屏

--- core/core-frontend/src/api/setting/sysParameter.ts
+++ core/core-frontend/src/api/setting/sysParameter.ts
@@ -2,3 +2,5 @@ import request from '@/config/axios'
 
 export const queryMapKeyApi = () => request.get({ url: '/sysParameter/queryOnlineMap' })
 export const saveMapKeyApi = data => request.post({ url: '/sysParameter/saveOnlineMap', data })
+export const subscription = (dvId, resourceId) =>
+  request.post({ url: `/subscription/${dvId}/${resourceId}` })
